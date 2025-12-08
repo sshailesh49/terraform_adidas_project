9e8ec674204284c231b87ff77d67fd24202b3626
Your Name <YourName@gmail.com>
2025-12-08 15:01:01 +0530

    Apply terraform fmt changes

--- infra/main.tf
+++ infra/main.tf
@@ -25,9 +25,9 @@ module "lambda" {
   project_name = var.project_name
 
 
-  lambda_s3_key_adidas = var.lambda_s3_key_adidas
-  lambda_s3_key_shopee = var.lambda_s3_key_shopee
-  lambda_s3_key_fareye = var.lambda_s3_key_fareye
+  lambda_s3_key_adidas     = var.lambda_s3_key_adidas
+  lambda_s3_key_shopee     = var.lambda_s3_key_shopee
+  lambda_s3_key_fareye     = var.lambda_s3_key_fareye
   lambda_layer_s3_key_fpdf = var.lambda_layer_s3_key_fpdf
 
 
