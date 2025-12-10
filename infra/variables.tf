c595d58bcf21f35cd67bc132b2285b17d47efefd
sshailesh49 <59621253+sshailesh49@users.noreply.github.com>
2025-12-10 18:52:26 +0530

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -56,16 +56,6 @@ variable "redshift_master_password" {
 }
 
 
-variable "aws_access_key" {
-  description = "AWS Access Key ID"
-  type        = string
-  sensitive   = true
-}
 
-variable "aws_secret_key" {
-  description = "AWS Secret Access Key"
-  type        = string
-  sensitive   = true
-}
 
 
