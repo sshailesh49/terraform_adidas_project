94c4ef6b00cdd8f74e3299e96b4408ef907b34ff
sshailesh49 <59621253+sshailesh49@users.noreply.github.com>
2025-12-10 23:34:58 +0530

    Update main.tf

--- infra/modules/apigw/main.tf
+++ infra/modules/apigw/main.tf
@@ -48,7 +48,9 @@ resource "aws_api_gateway_integration" "adidas_lambda_integration" {
   http_method             = aws_api_gateway_method.post_adidas.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = var.adidas_invoke_arn
+  #uri                     = var.adidas_invoke_arn
+  uri = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.adidas.arn}/invocations"
+
   
 }
 
